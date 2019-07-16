352ae532fa263c4431f7810d6fd750dd5431372e
brandnn <brandnn@uw.edu>
2019-07-16 12:52:06 -0700

    initilizing javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+//SET VARIABLES
+var characterSelected = false;
+
+var defenderSelected = false;
+
+var character = {};
+
+var defender = {};
+
+var enemiesDefeated = 0;
+
+gameOver = false;
+
+
+var jarJarBinks = {
+    name: "Jar Jar Binks",
+    health: 120,
+    attack: 8,
+    attackPower: 8
+  };
+  
+  var lukeSkywalker = {
+    name: "Luke Skywalker",
+    health: 100,
+    attack: 5,
+    attackPower: 5
+};
+  
+  var darthVader = {
+    name: "Darth Vader",
+    health: 150,
+    attack: 20,
+    attackPower: 20
+  };
+  
+  var R2D2 = {
+    name: "R2D2",
+    health: 70,
+    attack: 40,
+    attackPower: 40
+  };
+  
+  var bobaFett = {
+    name: "Boba Fett",
+    health: 180,
+    attack: 25,
+    attackPower: 25
+  };
+  
