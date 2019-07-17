aff5bd1e992bcd1ea2711e47047421ecbde83847
brandnn <brandnn@uw.edu>
2019-07-16 17:09:42 -0700

    modified game code for hp and atk power

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ gameOver = false;
 
 var jarJarBinks = {
     name: "Jar Jar Binks",
-    health: 420,
+    health: 421,
     attack: 1,
     attackPower: 1
   };
@@ -24,23 +24,23 @@ var jarJarBinks = {
   
   var darthVader = {
     name: "Darth Vader",
-    health: 150,
+    health: 250,
     attack: 20,
     attackPower: 20
   };
   
   var R2D2 = {
     name: "R2D2",
-    health: 70,
+    health: 60,
     attack: 40,
     attackPower: 40
   };
   
   var bobaFett = {
     name: "Boba Fett",
-    health: 180,
-    attack: 25,
-    attackPower: 25
+    health: 130,
+    attack: 15,
+    attackPower: 15
   };
   
   function initHero(selectedChar) {
@@ -96,30 +96,24 @@ var jarJarBinks = {
     $("#jar-jar-char").on("click", function () {
       console.log("Jar Jar Binks chosen");
   
-      // User is choosing the character
       if(charSelected == false) {
         $("#announcement").empty();
-  
-        // Set the user's character
+
         initHero(jarJarBinks);
         charSelected = true;
   
-        // Display the chosen character
         $("#jar-jar-char").removeClass("open-char").addClass("chosenChar");
         $("#chosenChar").append(this);
-  
-        // Move the remaining characters to the enemies section
+        
         moveToEnemies();
       } else if ((charSelected == true) && (enemySelected == false)) {
-        // User is choosing the defender
+   
         if($("#jar-jar-char").hasClass("enemyChar")) {
           $("#announcement").empty();
   
-          // Set the user's enemy
           initEnemy(jarJarBinks);
           enemySelected = true;
   
-          // Add the character to the defender section
           $("#jar-jar-char").removeClass("enemyChar").addClass("enemyDefender");
           $("#defendArea").append(this);
         }
@@ -129,30 +123,24 @@ var jarJarBinks = {
       $("#luke-sky-char").on("click", function () {
         console.log("Luke Skywalker is selected");
     
-        // User is choosing the character
         if(charSelected== false) {
           $("#announcement").empty();
     
-          // Set the user's character
           initHero(lukeSkywalker);
           charSelected= true;
     
-          // Display the chosen character
           $("#luke-sky-char").removeClass("open-char").addClass("chosenChar");
           $("#chosenChar").append(this);
     
-          // Move the remaining characters to the enemies section
           moveToEnemies();
         } else if ((charSelected== true) && (enemySelected == false)) {
-          // User is choosing the defender
+
           if($("#luke-sky-char").hasClass("enemyChar")) {
             $("#announcement").empty();
     
-            // Set the user's enemy
             initEnemy(lukeSkywalker);
             enemySelected = true;
     
-            // Add the character to the defender section 
             $("#luke-sky-char").removeClass("enemyChar").addClass("enemyDefender");
             $("#defendArea").append(this);
           }
@@ -162,30 +150,24 @@ var jarJarBinks = {
       $("#darth-vader-char").on("click", function () {
         console.log("Darth Vader is selected");
     
-        // User is choosing the character
         if(charSelected== false) {
           $("#announcement").empty();
     
-          // Set the user's character
           initHero(darthVader);
           charSelected= true;
     
-          // Display the chosen character
           $("#darth-vader-char").removeClass("open-char").addClass("chosenChar");
           $("#chosenChar").append(this);
     
-          // Move the remaining characters to the enemies section
           moveToEnemies();
         } else if ((charSelected== true) && (enemySelected == false)) {
-          // User is choosing the defender
+
           if($("#darth-vader-char").hasClass("enemyChar")) {
             $("#announcement").empty();
-    
-            // Set the user's enemy
+
             initEnemy(darthVader);
             enemySelected = true;
     
-            // Add the character to the defender section 
             $("#darth-vader-char").removeClass("enemyChar").addClass("enemyDefender");
             $("#defendArea").append(this);
           }
@@ -195,30 +177,24 @@ var jarJarBinks = {
       $("#r2d2-char").on("click", function () {
         console.log("Artoo Detoo chosen");
     
-        // User is choosing the character
         if(charSelected== false) {
           $("#announcement").empty();
     
-          // Set the user's character
           initHero(R2D2);
           charSelected= true;
     
-          // Display the chosen character
           $("#r2d2-char").removeClass("open-char").addClass("chosenChar");
           $("#chosenChar").append(this);
     
-          // Move the remaining characters to the enemies section
           moveToEnemies();
         } else if ((charSelected== true) && (enemySelected == false)) {
-          // User is choosing the defender
+
           if($("#r2d2-char").hasClass("enemyChar")) {
             $("#announcement").empty();
     
-            // Set the user's enemy
             initEnemy(R2D2);
             enemySelected = true;
     
-            // Add the character to the defender section 
             $("#r2d2-char").removeClass("enemyChar").addClass("enemyDefender");
             $("#defendArea").append(this);
           }
@@ -228,30 +204,24 @@ var jarJarBinks = {
       $("#boba-char").on("click", function () {
         console.log("Boba Fett chosen");
     
-        // User is choosing the character
         if(charSelected== false) {
           $("#announcement").empty();
     
-          // Set the user's character
-          initHero(R2D2);
+          initHero(bobaFett);
           charSelected= true;
     
-          // Display the chosen character
           $("#boba-char").removeClass("open-char").addClass("chosenChar");
           $("#chosenChar").append(this);
-    
-          // Move the remaining characters to the enemies section
+
           moveToEnemies();
         } else if ((charSelected== true) && (enemySelected == false)) {
-          // User is choosing the defender
+
           if($("#boba-char").hasClass("enemyChar")) {
             $("#announcement").empty();
     
-            // Set the user's enemy
-            initEnemy(R2D2);
+            initEnemy(bobaFett);
             enemySelected = true;
     
-            // Add the character to the defender section 
             $("#boba-char").removeClass("enemyChar").addClass("enemyDefender");
             $("#defendArea").append(this);
           }
@@ -261,22 +231,18 @@ var jarJarBinks = {
     
   $("#attack").on("click", function() {
 
-    // User is ready to attack the defender
     if (charSelected&& enemySelected && !gameOver) {
-      // User attacks the defender and decreases the defender's health points
+
       enemy.health = enemy.health - character.attack;
       $(".enemyDefender").children(".health").html(enemy.health);
       $("#announcement").html("<br><p>You attacked " + enemy.name + " for " + character.attack + " damage.<p>");
 
-      // User's attack power increases
       character.attack = character.attack + character.attackPower;
 
-      // If defender is still alive, they counter attack the user
       if (enemy.health > 0) {
         character.health = character.health - enemy.attackPower;
         $(".chosenChar").children(".health").html(character.health);
 
-        // Check if the user survives the attack
         if (character.health > 0) {
           $("#announcement").append("<br><p>" + enemy.name + " attacked you back for " + enemy.attackPower + " damage.</p>");
         } else {
@@ -286,14 +252,14 @@ var jarJarBinks = {
           
         }
       } else {
-        // Defender is defeated
+
         enemiesDefeated++;
         enemySelected = false;
         $(".enemyDefender").hide();
-        $("#announcement").html("<br><p>You demolished " + enemy.name + "! Pick another victim.</p>");
+        $("#announcement").html("<br><p>You attacked " + enemy.name + " for " + character.attack + " damage.<p>");
+        $("#announcement").append("<br><p>You demolished " + enemy.name + "! Pick another victim.</p>");
         
 
-        // Check if the user has won the game
         if (enemiesDefeated === 4) {
           gameOver = true;
           $("#announcement").html("<br><p>You win Star Wars!</p><p>Play again?</p>");
